cdd28c00e2510bec5c8163239e5ccdda584c4eb7
Dave Powell <poweld@gmail.com>
2014-06-16 14:14:48 +0000

    test webhook location now configurable

--- src/test/scala/WebhooksSpec.scala
+++ src/test/scala/WebhooksSpec.scala
@@ -7,13 +7,15 @@ class WebhooksSpec extends FlatSpec {
   val config = ConfigFactory.load
   val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
   val TEST_ROOM_KEY = "com.imadethatcow.hipchat.test_room"
+  val TEST_WEBHOOK_KEY = "com.imadethatcow.hipchat.test_webhook"
   val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
   val testRoomTry = Try(config.getString(TEST_ROOM_KEY))
+  val testWebhookTry = Try(config.getString(TEST_WEBHOOK_KEY))
   if (apiTokenTry.isFailure) fail("Could not find api_token in config")
   if (testRoomTry.isFailure) fail("Could not find test_room in config")
+  if (testWebhookTry.isFailure) fail("Could not find test_webhook in config")
 
-  for (apiToken <- apiTokenTry; room <- testRoomTry) {
-    val url = "http://ec2-54-85-170-167.compute-1.amazonaws.com/webhook"
+  for (apiToken <- apiTokenTry; room <- testRoomTry; url <- testWebhookTry) {
     val webhooks = new Webhooks(apiToken)
     val event = WebhookEvent.room_message
 
