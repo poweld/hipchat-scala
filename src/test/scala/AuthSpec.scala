f1b70f32ddbbe075bc98f60fdc759f8c617daef1
mdotson <mdotson@mediamath.com>
2014-07-03 12:57:50 -0500

    password auth partway done

--- src/test/scala/AuthSpec.scala
+++ src/test/scala/AuthSpec.scala
@@ -5,21 +5,32 @@ import scala.util.Try
 
 class AuthSpec extends FlatSpec with Matchers {
 
-  // this can work, but it is probably not good to test external API authentication
+  val config = ConfigFactory.load
+  val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
+  val USERNAME = "com.imadethatcow.hipchat.username"
+  val PASSWORD = "com.imadethatcow.hipchat.password"
+  val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
+  val usernameTry = Try(config.getString(USERNAME))
+  val passwordTry = Try(config.getString(PASSWORD))
+  if (apiTokenTry.isFailure) fail("Could not find api_token in config")
+  if (usernameTry.isFailure) fail("Could not find username in config")
+  if (passwordTry.isFailure) fail("Could not find password in config")
 
-//  val config = ConfigFactory.load
-//  val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
-//  val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
-//  if (apiTokenTry.isFailure) fail("Could not find api_token in config")
-//
-//  for (apiToken <- apiTokenTry) {
-//     val auth = new Auth(apiTokenTry.get)
-//
-//     "Auth create personal request" should "return a valid JSON response" in {
-//       for (authResponse <- auth.generatePersonalToken()) {
-//         println(authResponse)
-//         authResponse.access_token should not equal apiTokenTry.get
-//       }
-//     }
-//  }
+  for (apiToken <- apiTokenTry; username <- usernameTry; password <- passwordTry) {
+    val auth = new Auth(apiTokenTry.get)
+
+    // this can work, but it is probably not good to test external API authentication
+
+//    "Auth create personal request" should "return a valid JSON response" in {
+//      for (authResponse <- auth.generatePersonalToken()) {
+//        println(authResponse)
+//        authResponse.access_token should not equal apiTokenTry.get
+//      }
+
+    "Auth create personal request" should "return a valid JSON response" in {
+      for (authResponse <- auth.generatePasswordToken(username, password)) {
+        println(authResponse)
+      }
+    }
+  }
 }
