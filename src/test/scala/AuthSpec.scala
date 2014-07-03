46882b95fc94c389f95343a716b7c0de8ab1b56e
mdotson <mdotson@mediamath.com>
2014-07-03 11:36:30 -0500

    fixing comment

--- src/test/scala/AuthSpec.scala
+++ src/test/scala/AuthSpec.scala
@@ -7,21 +7,19 @@ class AuthSpec extends FlatSpec with Matchers {
 
   // this can work, but it is probably not good to test external API authentication
 
-  /*
-  val config = ConfigFactory.load
-  val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
-  val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
-  if (apiTokenTry.isFailure) fail("Could not find api_token in config")
-
-  for (apiToken <- apiTokenTry) {
-     val auth = new Auth(apiTokenTry.get)
-
-     "Auth create personal request" should "return a valid JSON response" in {
-       for (authResponse <- auth.generatePersonalToken()) {
-         println(authResponse)
-         authResponse.access_token should not equal apiTokenTry.get
-       }
-     }
-  }
-  */
+//  val config = ConfigFactory.load
+//  val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
+//  val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
+//  if (apiTokenTry.isFailure) fail("Could not find api_token in config")
+//
+//  for (apiToken <- apiTokenTry) {
+//     val auth = new Auth(apiTokenTry.get)
+//
+//     "Auth create personal request" should "return a valid JSON response" in {
+//       for (authResponse <- auth.generatePersonalToken()) {
+//         println(authResponse)
+//         authResponse.access_token should not equal apiTokenTry.get
+//       }
+//     }
+//  }
 }
