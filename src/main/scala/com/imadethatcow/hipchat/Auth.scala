56cbda8892b4026d0532f1d48ee4795e1663c4d8
mdotson <mdotson@mediamath.com>
2014-07-03 12:57:50 -0500

    password auth partway done

--- src/main/scala/com/imadethatcow/hipchat/Auth.scala
+++ src/main/scala/com/imadethatcow/hipchat/Auth.scala
@@ -3,8 +3,8 @@ package com.imadethatcow.hipchat
 import com.imadethatcow.hipchat.common.Common._
 import org.slf4j.LoggerFactory
 
-import scala.util.{Failure, Success, Try}
-import com.imadethatcow.hipchat.common.caseclass.{AuthRequest, AuthResponse}
+import scala.util.Try
+import com.imadethatcow.hipchat.common.caseclass.AuthResponse
 import scala.util.Success
 import scala.util.Failure
 import scala.Some
@@ -20,6 +20,33 @@ class Auth(private[this] val apiToken: String) {
       .setHeader("Content-Type", "application/x-www-form-urlencoded")
 
     val urlEncodedVals = Seq(("grant_type", AuthGrantType.personal.toString))
+
+    val reqWithBody = addFormUrlEncodedVals(req, urlEncodedVals:_*)
+
+    val jsonOpt = resolveRequest(reqWithBody)
+
+    jsonOpt match {
+      case Some(json) =>
+        val authResponse = Try(mapper.readValue(json, classOf[AuthResponse]))
+        authResponse match {
+          case Success(v) =>
+            Some(v)
+          case Failure(e) =>
+            log.error("Failed to parse JSON response", e)
+            None
+        }
+      case None =>
+        None
+    }
+  }
+
+  def generatePasswordToken(username: String,
+                            password: String) = {
+    val req = addToken(Auth.urlPost, apiToken)
+      .setHeader("Content-Type", "application/x-www-form-urlencoded")
+
+    val urlEncodedVals = Seq("grant_type" -> AuthGrantType.password.toString,
+      "username" -> username, "password" -> password)
 
     val reqWithBody = addFormUrlEncodedVals(req, urlEncodedVals:_*)
 
