34bde8f3b6900710398771707a7b6d2507e2aef7
mdotson <mdotson@mediamath.com>
2014-07-10 18:51:01 -0500

    get session done

--- src/main/scala/com/imadethatcow/hipchat/common/caseclass/Auth.scala
+++ src/main/scala/com/imadethatcow/hipchat/common/caseclass/Auth.scala
@@ -2,6 +2,7 @@ package com.imadethatcow.hipchat.common.caseclass
 
 import com.imadethatcow.hipchat.common.enums.AuthGrantType
 import AuthGrantType.AuthGrantType
+import com.imadethatcow.hipchat.common.enums.OwnerType.OwnerType
 
 case class AuthRequest(grant_type: AuthGrantType,
                        username: Option[String] = None,
@@ -10,6 +11,7 @@ case class AuthRequest(grant_type: AuthGrantType,
                        scope: Option[String] = None,
                        password: Option[String] = None,
                        refresh_token: Option[String] = None)
+
 case class AuthResponse(access_token: String,
                         expires_in: Long,
                         group_name: String,
@@ -17,3 +19,16 @@ case class AuthResponse(access_token: String,
                         scope: String,
                         group_id: String,
                         refresh_token: Option[String] = None)
+
+case class GetSessionResponse(scopes: Seq[String],
+                              access_token: String,
+                              expires_in: Long,
+                              client: AuthClient,
+                              owner: Option[UsersItem],
+                              owner_type: String)
+
+case class AuthClient(room: Option[RoomsItem],
+                      id: String,
+                      allowed_scopes: Seq[String],
+                      name: Option[String],
+                      oauth_client_id: String)
---
+++ src/main/scala/com/imadethatcow/hipchat/common/enums/OwnerType.scala
@@ -0,0 +1,6 @@
+package com.imadethatcow.hipchat.common.enums
+
+object OwnerType extends Enumeration {
+  type OwnerType = Value
+  val client, user = Value
+}
