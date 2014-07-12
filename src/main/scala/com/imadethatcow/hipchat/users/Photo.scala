d2799c2e695ee5678394ed282f7e23b6b9075944
mdotson <mdotson@Helenas-iMac.local>
2014-07-12 12:37:15 -0500

    partly implemented photo

---
+++ src/main/scala/com/imadethatcow/hipchat/users/Photo.scala
@@ -0,0 +1,36 @@
+package com.imadethatcow.hipchat.users
+
+import com.imadethatcow.hipchat.common.Common
+import Common._
+
+class Photo(private[this] val apiToken: String) {
+  def update(idOrEmail: String) = {
+    val req = addToken(Photo.urlPut(idOrEmail), apiToken)
+  }
+}
+
+object Photo {
+  private def url(idOrEmail: String) = apiUrl / "user" / idOrEmail / "photo"
+
+  def urlPut(idOrEmail: String) = url(idOrEmail).PUT
+  def urlDelete(idOrEmail: String) = url(idOrEmail).DELETE
+}
+
+/*
+class PrivateMessenger(private[this] val apiToken: String) extends Logging {
+  def sendMessage(idOrEmail: String, message: String): Boolean = {
+    val req = addToken(PrivateMessenger.url(idOrEmail), apiToken)
+      .setBody(mapper.writeValueAsString(PrivateMessage(message)))
+      .setHeader("Content-Type", "application/json")
+
+    resolveRequest(req, 204) match {
+      case Some(r) => true
+      case None => false
+    }
+  }
+}
+
+object PrivateMessenger {
+  def url(idOrEmail: String) = (apiUrl / "user" / idOrEmail / "message").POST
+}
+*/
