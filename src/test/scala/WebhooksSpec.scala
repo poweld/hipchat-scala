79927e5f5c42348556fcdcb75b80f4cfd32078f4
poweld <poweld@gmail.com>
2014-07-13 12:17:59 -0400

    removed unnecessary configuration elements from testing

--- src/test/scala/WebhooksSpec.scala
+++ src/test/scala/WebhooksSpec.scala
@@ -8,15 +8,13 @@ class WebhooksSpec extends FlatSpec {
   val config = ConfigFactory.load
   val API_TOKEN_KEY = "com.imadethatcow.hipchat.auth_token"
   val TEST_ROOM_KEY = "com.imadethatcow.hipchat.test_room"
-  val TEST_WEBHOOK_KEY = "com.imadethatcow.hipchat.test_webhook"
   val apiTokenTry = Try(config.getString(API_TOKEN_KEY))
   val testRoomTry = Try(config.getString(TEST_ROOM_KEY))
-  val testWebhookTry = Try(config.getString(TEST_WEBHOOK_KEY))
   if (apiTokenTry.isFailure) fail("Could not find api_token in config")
   if (testRoomTry.isFailure) fail("Could not find test_room in config")
-  if (testWebhookTry.isFailure) fail("Could not find test_webhook in config")
 
-  for (apiToken <- apiTokenTry; room <- testRoomTry; url <- testWebhookTry) {
+  for (apiToken <- apiTokenTry; room <- testRoomTry) {
+		val url = "http://www.imadethatcow.com"
     val webhooks = new Webhooks(apiToken)
     val event = WebhookEvent.room_message
 
