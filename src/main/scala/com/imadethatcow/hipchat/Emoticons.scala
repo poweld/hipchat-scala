0f1dfe204eb98b5c5904443cf51470b466a227a0
mdotson <mdotson@mediamath.com>
2014-07-29 11:34:29 -0500

    fix emoticons and emoticons test

--- src/main/scala/com/imadethatcow/hipchat/Emoticons.scala
+++ src/main/scala/com/imadethatcow/hipchat/Emoticons.scala
@@ -2,17 +2,23 @@ package com.imadethatcow.hipchat
 
 import com.imadethatcow.hipchat.common.Common._
 import com.imadethatcow.hipchat.common.Logging
-import com.imadethatcow.hipchat.common.caseclass.{EmoticonDetails, Emoticon, EmoticonsResponse}
 import scala.concurrent.{ExecutionContext, Future}
+import com.imadethatcow.hipchat.common.caseclass.Emoticon
+import com.imadethatcow.hipchat.common.caseclass.EmoticonDetails
+import com.imadethatcow.hipchat.common.caseclass.EmoticonsResponse
+import com.imadethatcow.hipchat.common.enums.EmoticonType
+import com.imadethatcow.hipchat.common.enums.EmoticonType.EmoticonType
 
 class Emoticons(private[this] val apiToken: String)(implicit executor: ExecutionContext) extends Logging {
-  def getAll(startIndex: Option[Long] = None,
-             maxResults: Option[Long] = None,
-             `type`: Option[Boolean] = None): Future[Seq[Emoticon]] = {
+  def getAll(startIndex: Int = 0,
+             maxResults: Int = 100,
+             `type`: EmoticonType = EmoticonType.all): Future[Seq[Emoticon]] = {
+    if (startIndex < 0) throw new IllegalArgumentException("startIndex must be greater than 0")
+    if (maxResults < 0 || maxResults > 100) throw new IllegalArgumentException("maxResults range: 0-100")
     var req = addToken(Emoticons.url, apiToken)
-    for (si <- startIndex) req = req.addQueryParameter("start-index", si.toString)
-    for (mr <- maxResults) req = req.addQueryParameter("max-results", mr.toString)
-    for (t <- `type`) req = req.addQueryParameter("type", t.toString)
+    req = req.addQueryParameter("start-index", startIndex.toString)
+    req = req.addQueryParameter("max-results", maxResults.toString)
+    req = req.addQueryParameter("type", `type`.toString)
 
     resolveAndDeserialize[EmoticonsResponse](req) map {
       response => response.items.map {
@@ -30,7 +36,7 @@ class Emoticons(private[this] val apiToken: String)(implicit executor: Execution
 object Emoticons {
   val url = (apiUrl / "emoticon").GET
   def url(emoticonIdOrName: Any) = emoticonIdOrName match {
-    case _: Long | _: String =>
+    case _: Long | _: String | _: Int =>
       (apiUrl / "emoticon" / emoticonIdOrName.toString).GET
   }
 }
